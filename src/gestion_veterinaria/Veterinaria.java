aace3ec62c8eb41d90db7698dd443e9eb55810fb
mari <n00443563@upn.pe>
2026-04-23 23:06:44 -0500

    Manejo de excepciones InputMismatchException en el menú

--- src/gestion_veterinaria/Veterinaria.java
+++ src/gestion_veterinaria/Veterinaria.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Veterinaria {
+    //Uso de Colecciones (ArrayList)
     private List<Mascota> listaMascotas;
 
     public Veterinaria() {
